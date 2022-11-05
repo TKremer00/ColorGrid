19b4609ab92d5bf1b8314b38bcc844261948da65
Tim Kremer <494033@student.saxion.nl>
2022-11-05 19:05:11 +0100

    changed code to use backtracking

--- main.js
+++ main.js
@@ -1,171 +1,125 @@
-const colorPicker = document.querySelector('#colorPicker');
-const submitButton = document.querySelector('#add');
-const colorDiv = document.querySelector("#colors");
-const colorCount = document.querySelector("#colorCount");
-const createGrid = document.querySelector("#create");
-const colorPickerDiv = document.querySelector("#colorDisplay");
-const errors = document.querySelector("#errors");
-const rowsDiv = document.querySelector("#rows");
-const columnsDiv = document.querySelector("#columns");
-
-let rows = 3;
-let columns = 3;
-
+const COLOR_GRID = []
+const SPACING = 30;
+const DRAW_SIZE = 50;
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext('2d');
-const margin = 30;
-const startPoint = 10;
-const size = 50;
-const maxTrys = 20;
-let trys = 0;
 
-let colorGrid = []
+const COLORS = [];
 
-let colorChange = false;
-let colorArray = [];
+let ROWS = 3;
+document.querySelector("#rows", "change", e => { ROWS = e.target.value; });
+let COLS = 3;
+document.querySelector("#cols", "change", e => { COLS = e.target.value; });
 
-colorPicker.addEventListener("change", (event) => {
-    colorChange = true;
-    colorPickerDiv.setAttribute('style',`background-color:${colorPicker.value};color:${colorPicker.value}`);
+document.querySelector("#colorDisplay").addEventListener("click", () => colorPicker.click());
+
+document.querySelector('#colorPicker').addEventListener("change", () => {
+    document.querySelector("#colorDisplay").setAttribute('style',`background-color:${colorPicker.value};color:${colorPicker.value}`);
 });
 
-colorPickerDiv.addEventListener("click", (event) => {
-    colorPicker.focus();
-    colorPicker.click();
-});
-
-submitButton.addEventListener("click", (event) => {
-    if(colorChange)
-    {
-        const color = {
-            hex:"",
-            rgb:[],
-        };
-        color.hex = colorPicker.value;
-        color.rgb = hexToRgb(color.hex);
-        colorArray.push(color);
-        colorChange = false;
-        colorCount.innerHTML = `${colorArray.length} colors selected`;
-        const div = document.createElement("DIV");
-        div.setAttribute('class', 'colorShow');
-        div.setAttribute('style',`background-color:${colorArray[colorArray.length - 1].hex}`);
-        colorDiv.appendChild(div);
-        colorPickerDiv.setAttribute('style',`background-color:white;color:black`);
-    }else {
-        alert('No color is selected');
-
-    }
-});
-
-createGrid.addEventListener("click", (event) => {
-    if(colorArray.length > 5) {
-        columns = columnsDiv.value;
-        rows = rowsDiv.value;
-        makeColorArray();
-        calculateCanvasSize();
-        drawGrid();
-        errors.innerHTML = "";
-    }else {
-        alert('Select at least 6 colors')
+document.querySelector('#add').addEventListener("click", (event) => {
+    const newColor = colorPicker.value;
+    if(!newColor || COLORS.includes(newColor)) {
+        alert("No color or duplicate color chosen");
+        return;
     }
 
-});
+    COLORS.push(newColor);
+    document.querySelector("#colorCount").innerHTML = `${COLORS.length} colors selected`;
 
-function makeColorArray() 
-{
-    colorGrid = [];
-    let impossible = false;
-    for (let i = 0; i < rows; i++) {
-        let columnColors = [];
-        let cantAdd = false; 
-        for (let j = 0; j < columns; j++) {
-            let colorToAdd = colorArray[Math.floor(Math.random() * colorArray.length)].hex;
-
-            let colorsChecked = 1;
-            let similar = true;
-            let checkedColors = [];
-            while (similar){
-                similar = false;
-                
-                // Check in 3x3 grid if similar
-                // check left 
-                if(j != 0) 
-                {
-                    if(columnColors[j - 1] === colorToAdd){               
-                        similar = true;
-                    }
-                }
-
-                if(i != 0) 
-                {
-                    // check top left
-                    if(colorGrid[i - 1][j - 1] === colorToAdd) {
-                        similar = true;
-                    }
-
-                    // check top
-                    if(colorGrid[i - 1][j] === colorToAdd) {
-                        similar = true;
-                    }
-
-                    // check top right
-                    if(j + 1 < columns && colorGrid[i - 1][j + 1] === colorToAdd) {
-                        similar = true;
-                    }
-                }
+    const div = document.createElement("div");
+    div.setAttribute('class', 'colorShow');
+    div.setAttribute('style',`background-color:${COLORS[COLORS.length - 1]}`);
     
-                colorsChecked++;
-                
-                if(similar) {
-                    checkedColors.push(colorToAdd);
-                    colorToAdd = colorArray[Math.floor(Math.random() * colorArray.length)].hex;
+    document.querySelector("#selectedColors").appendChild(div);
+    document.querySelector('#colorDisplay').setAttribute('style',`background-color:white;color:black`);
+});
 
-                    while(checkedColors.includes(colorToAdd)) 
-                    {
-                        colorToAdd = colorArray[Math.floor(Math.random() * colorArray.length)].hex;
-                    }
-                }
+document.querySelector("#create").addEventListener("click", e => {
+    if(COLORS.length < 6) {
+        alert('Select at least 6 colors');
+        return;
+    }
 
-                if(colorsChecked == colorArray.length && similar) 
-                {
-                    similar = false;
-                    impossible = true;
-                }
+    fillGrid();
+    let _ = solve();
+    drawGrid();
+});
+
+function fillGrid() {
+    for (let i = 0; i < ROWS; i++) {
+        let row = [];
+        COLOR_GRID.push(row);
+        for (let j = 0; j < COLS; j++) {
+            row.push(undefined);
+        }
+    }
+}
+
+function validateColorAtPosition(row, column, color) {
+    for (let i = row - 1; i <= row + 1; i++) {
+        if(i < 0 || i >= ROWS) continue;
+
+        for (let j = column - 1; j <= column + 1; j++) {
+            if((j < 0 || j >= COLS) || (j == column && i == row)) continue;
+            const colorAtColumn = COLOR_GRID[i][j];
+            
+            if(colorAtColumn == color) {
+                return false;
             }
-            columnColors.push(colorToAdd);
         }
-        if(impossible && trys < maxTrys) {
-            trys++;
-            makeColorArray(rows,columns);
-        }
-        colorGrid.push(columnColors);
     }
+
+    return true;
 }
 
-function drawGrid() 
-{
-    for (let i = 0; i < rows; i++) {
-        for (let j = 0; j < columns; j++) {
+function findEmpty() {
+    for (let i = 0; i < COLOR_GRID.length; i++) {
+        for (let j = 0; j < COLOR_GRID[i].length; j++) {
+            if(COLOR_GRID[i][j] == undefined) {
+                return [i, j];
+            }
+            
+        }
+    }
+    return undefined;
+}
+
+function solve() {
+    const emptyField = findEmpty();
+
+    if(!emptyField) {
+        return true;
+    }
+    
+    const [row, column] = emptyField;
+
+    for (let i = 0; i < COLORS.length; i++) {
+        if(validateColorAtPosition(row, column, COLORS[i])) {
+            COLOR_GRID[row][column] = COLORS[i];
+
+            if(solve()) {
+                return true;
+            }
+
+            COLOR_GRID[row][column] = undefined;
+        }
+    }
+
+    return false;
+}
+
+function drawGrid() {
+    canvas.setAttribute("width", (COLS * DRAW_SIZE) + (COLS * SPACING));
+    canvas.setAttribute("height", (ROWS * DRAW_SIZE) + (ROWS * SPACING));
+
+    for (let i = 0; i < ROWS; i++) {
+        for (let j = 0; j < COLS; j++) {
             ctx.beginPath();
-            ctx.fillStyle = colorGrid[i][j];
-            ctx.fillRect(startPoint + ((size + margin) * j),startPoint + ((size + margin) * i),size,size);
+            ctx.fillStyle = COLOR_GRID[i][j];
+            console.log("x", (DRAW_SIZE + SPACING) * j);
+            console.log("y", (DRAW_SIZE + SPACING) * i);
+            ctx.fillRect((DRAW_SIZE + SPACING) * j, (DRAW_SIZE + SPACING) * i, DRAW_SIZE, DRAW_SIZE);
         }
-    }
-}
-
-function hexToRgb(hex) {
-    var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
-
-    return result ? {
-      r: parseInt(result[1], 16),
-      g: parseInt(result[2], 16),
-      b: parseInt(result[3], 16)
-    } : null;
-}
-
-function calculateCanvasSize() {
-    let w = (columns * size) + (columns * margin) - startPoint;
-    let h = (rows * size) + (rows * margin) - startPoint;
-    canvas.setAttribute("width", w);
-    canvas.setAttribute("height", h);
+    }    
 }
